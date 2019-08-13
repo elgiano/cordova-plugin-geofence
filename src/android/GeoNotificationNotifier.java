b39fbeaab6203b91e53a536a55760b97564ad76d
elgiano <elgiano@gmail.com>
2019-08-13 03:54:03 +0200

    test notification channel

--- src/android/GeoNotificationNotifier.java
+++ src/android/GeoNotificationNotifier.java
@@ -1,6 +1,7 @@
 package com.cowbell.cordova.geofence;
 
 import android.app.NotificationManager;
+import android.app.NotificationChannel ;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -17,11 +18,28 @@ public class GeoNotificationNotifier {
     private BeepHelper beepHelper;
     private Logger logger;
 
+    private void createNotificationChannel(NotificationManager notificationManager) {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          CharSequence name = 'test-channel';
+          String description = 'test-channel-description';
+          int importance = NotificationManager.IMPORTANCE_DEFAULT;
+          NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+          channel.setDescription(description);
+          // Register the channel with the system; you can't change the importance
+          // or other notification behaviors after this
+          notificationManager.createNotificationChannel(channel);
+      }
+    }
+
+
     public GeoNotificationNotifier(NotificationManager notificationManager, Context context) {
         this.notificationManager = notificationManager;
         this.context = context;
         this.beepHelper = new BeepHelper();
         this.logger = Logger.getLogger();
+        this.createNotificationChannel();
     }
 
     public void notify(Notification notification) {
