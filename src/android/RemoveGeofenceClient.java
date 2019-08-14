e2fd579acb02a07af2548907dfbd0c5b65b8f71a
elgiano <elgiano@gmail.com>
2019-08-14 15:27:45 +0200

    mispaste

--- src/android/RemoveGeofenceClient.java
+++ src/android/RemoveGeofenceClient.java
@@ -1,44 +1,60 @@
 package com.cowbell.cordova.geofence;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.util.Log;
 
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
 
 import java.util.List;
 
-public class RemoveGeofenceCommand extends AbstractGoogleServiceCommand {
+public class RemoveGeofenceClient  extends AbstractGoogleServiceCommand{
     private List<String> geofencesIds;
+    private PendingIntent pendingIntent;
+    private GeofencingClient client;
 
-    public RemoveGeofenceCommand(Context context, List<String> geofencesIds) {
+    public AddGeofenceClient(Context context,  List<String> geofencesIds){
         super(context);
+        this.client = LocationServices.getGeofencingClient(context);
         this.geofencesIds = geofencesIds;
     }
 
     @Override
-    protected void ExecuteCustomCode() {
-        if (geofencesIds != null && geofencesIds.size() > 0) {
-            logger.log(Log.DEBUG, "Removing geofences...");
-            LocationServices.GeofencingApi
-                .removeGeofences(mGoogleApiClient, geofencesIds)
-                .setResultCallback(new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        if (status.isSuccess()) {
-                            logger.log(Log.DEBUG, "Geofences successfully removed");
-                            CommandExecuted();
-                        } else {
-                            String message = "Removing geofences failed - " + status.getStatusMessage();
-                            logger.log(Log.ERROR, message);
-                            CommandExecuted(new Error(message));
+    public void ExecuteCustomCode() {
+      if (geofencesIds != null && geofencesIds.size() > 0) {
+          logger.log(Log.DEBUG, "Removing geofences...");
+            this.client
+                    .removeGeofences(geofencesIds)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                          logger.log(Log.DEBUG, "Geofences successfully removed");
+                          CommandExecuted();
                         }
-                    }
-                });
-        } else {
-            logger.log(Log.DEBUG, "Tried to remove Geofences when there were none");
-            CommandExecuted();
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                          String message = "Removing geofences failed - " + ((ApiException)e).getMessage();
+                          logger.log(Log.ERROR, message);
+                          CommandExecuted(new Error(message));
+                        }
+                    });
+
+        }catch (SecurityException exception){
+            logger.log(LOG.ERROR, "SecurityException while adding geofences");
+            exception.printStackTrace();
+            CommandExecuted(exception);
+        } catch (Exception exception) {
+            logger.log(LOG.ERROR, "Exception while adding geofences");
+            exception.printStackTrace();
+            CommandExecuted(exception);
         }
     }
 }
